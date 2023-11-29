282d5181c07ea8c9c22e87689e3052a5671a9a0c
shanghanrun <idim7@naver.com>
2023-11-30 01:06:57 +0900

    ..

--- lib/srcren/home.dart
+++ lib/srcren/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sqflite_ex02/srcren/edit.dart';
+import 'package:sqflite_ex02/controller/dbcontroller.dart';
+import 'package:sqflite_ex02/model/memo.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,22 +9,64 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('메모')),
-      body: Center(
-        child: Column(
-          children: [
-            Container(height: 50, color: Colors.pink),
-            Container(height: 50, color: Colors.amber),
-          ],
-        ),
+      body: Row(
+        children: [
+          const Padding(
+              padding: EdgeInsets.only(left: 20, top: 40, bottom: 20),
+              child: Text(
+                '메모',
+                style: TextStyle(fontSize: 36, color: Colors.blue),
+              )),
+          memoBuilder(),
+        ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-          label:Text('메모추가'),
+          label: const Text('메모추가'),
           icon: const Icon(Icons.add),
           onPressed: () {
             Navigator.push(
                 context, MaterialPageRoute(builder: (context) => EditPage()));
           }),
     );
+  }
+
+  List<Widget> loadMemo() {
+    // 아직 무슨용도인지 모르겠다...
+    List<Widget> memoList = [];
+    memoList.add(Container(
+      color: Colors.purpleAccent,
+      height: 100,
+    ));
+    memoList.add(Container(
+      color: Colors.redAccent,
+      height: 100,
+    ));
+
+    return memoList;
+  }
+
+  Future<List<Memo>> getMemosFromDB() async {
+    //반환값이 나중이라서 Future
+    DBController dbController = DBController();
+    return await dbController.memos(); //List<Memo>
+  }
+
+  Widget memoBuilder() {
+    return FutureBuilder(
+        future: getMemosFromDB(),
+        builder: (context, snap) {
+          if (snap.connectionState == ConnectionState.none &&
+              snap.hasData == null) {
+            return Container();
+          }
+          return ListView.builder(
+              itemCount: snap.data!.length,
+              itemBuilder: (context, i) {
+                Memo memo = snap.data![i];
+                return const Column(
+                  children: [],
+                );
+              });
+        });
   }
 }
